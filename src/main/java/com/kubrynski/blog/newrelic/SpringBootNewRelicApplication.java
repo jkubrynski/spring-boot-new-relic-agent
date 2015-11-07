f45e1fbcccaa409668f281e95ca0cc012b226a25
Jakub Kubrynski <jkubrynski@gmail.com>
2015-11-07 16:35:19 +0100

    Initial version

---
+++ src/main/java/com/kubrynski/blog/newrelic/SpringBootNewRelicApplication.java
@@ -0,0 +1,12 @@
+package com.kubrynski.blog.newrelic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNewRelicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNewRelicApplication.class, args);
+    }
+}
